46392f9ba349ab4eebf732db635685662fcda586
alexandrov-oleg <oleg.alexandrov.by@gmail.com>
2012-11-15 23:30:55 -0800

    renamed:    install1.sh -> install3.sh
    	renamed:    install1.sh -> install3.sh

--- install3.sh
+++ install3.sh
@@ -1,9 +1,13 @@
-1package="SugarEnt-6.5.7"
-installed="Ent_6572"
-build_url="http:33//honey-b/builds/6.5.7/latest"
+package="SugarEnt-6.5.7"
+installed="Ent_657"
+build_url="http://honey-b/builds/6.5.7/latest"
 
 #wget $build_url/$package.zip
 unzip -d curr $package.zip
 mv curr/Sugar* $installed
 rm -rf curr
 chown -R daemon:daemon $installed
+
+adfasdfr
+:wq
+
