6178604c4f84d5984579fe0ca5882888addb457d
alexandrov-oleg <oleg.alexandrov.by@gmail.com>
2012-11-16 04:05:20 -0800

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,16 @@
-package="SugarEnt-6.5.7"
-installed="Ent_657"
-build_url="http://honey-b/builds/6.5.7/latest"
+#package="SugarEnt-6.5.7"
+#installed="Ent_657"
+build_url=$1
+echo "build_url="$build_url
 
+archive=`echo $build_url | grep -oE "[^/]+$"`
+package=`echo $archive | grep -oE "[^.]+$"`
+echo "package="$package
+
+#wget http://honey-b/builds/65_Joneses/latest/SugarEnt-65_Joneses.zip
 #wget $build_url/$package.zip
-unzip -d curr $package.zip
-mv curr/Sugar* $installed
-rm -rf curr
-chown -R daemon:daemon $installed
+
+#unzip -d curr $package.zip
+#mv curr/Sugar* $installed
+#rm -rf curr
+#chown -R daemon:daemon $installed
